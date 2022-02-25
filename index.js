9a9e17f5c122b1cd839014864c7bd0044f1ca4e3
Allison <amh03160@gmail.com>
2022-02-25 16:31:18 -0600

    commit tree bdf0952b99a61856326370f757fc2b6d2298ab43

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+function superbowlWin(array){
+   //iterate through the array, search for win
+let winner = array.find(isAWin); 
+//return the year of the win or undefined if none
+    if (winner!== undefined) {
+        return winner["year"]
+    } else return undefined;
+ 
+};
+
+function isAWin(obj){
+   return obj["result"] === "W"
+}
+
+console.log(superbowlWin(record)); 
